c38e31a362423e30840abb2d1df055fc18e79b3f
Matheus Alves <mathalves07.winstore@outlook.com>
2023-11-04 21:35:30 -0300

    Created the base scss file and included the icons module needed
    resolves #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { IconsComponentTsComponent } from './icons.component.ts/icons.component.ts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IconsComponentTsComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/icons.component.ts/icons.component.ts.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-icons.component.ts',
+  templateUrl: './icons.component.ts.component.html',
+  styleUrls: ['./icons.component.ts.component.scss']
+})
+export class IconsComponentTsComponent {
+
+}
